a2943c35aca1ae3b6cc0241c5fb8fedfc459343d
MsRapunzel <anastasya.zolotova11@gmail.com>
2024-04-22 11:27:42 +0300

    Initial Commit

---
+++ CryptoiOS/CryptoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoiOSApp.swift
+//  CryptoiOS
+//
+//  Created by Anastasiia Zolotova on 22.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
