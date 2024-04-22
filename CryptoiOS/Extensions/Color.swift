49323a8a5dacc03d315573b70cea3a623e1984a2
MsRapunzel <anastasya.zolotova11@gmail.com>
2024-04-22 11:54:47 +0300

    set color scheme

---
+++ CryptoiOS/Extensions/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  CryptoiOS
+//
+//  Created by Anastasiia Zolotova on 22.04.2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
