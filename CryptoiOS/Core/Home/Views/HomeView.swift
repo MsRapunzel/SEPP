8cc72205d1bf062e6368764bc85a76d609bed858
MsRapunzel <anastasya.zolotova11@gmail.com>
2024-04-24 16:43:38 +0300

    feat: add a data model and View to display market statistics

--- CryptoiOS/Core/Home/Views/HomeView.swift
+++ CryptoiOS/Core/Home/Views/HomeView.swift
@@ -23,6 +23,7 @@ struct HomeView: View {
             VStack {
                 
                 homeHeader
+                HomeStatsView(showPortfolio: $showPortfolio)
                 SearchBarView(searchText: $vm.searchText)
                 columnTitles
                 if !showPortfolio {
