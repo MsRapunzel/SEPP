6c812ec4298c377953182b9beb3597a30a3e80d0
Anastasiia Zolotova <anastasya.zolotova11@gmail.com>
2024-04-27 21:08:48 +0300

    feat: Implement a search bar

--- CryptoiOS/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoiOS/Core/Home/ViewModels/HomeViewModel.swift
@@ -28,6 +28,16 @@ class HomeViewModel: ObservableObject {
     
     func addSubscriber() {
         
+        // updates allCoins
+        $searchText
+            .combineLatest(coinDataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+        
         coinDataService.$allCoins
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
@@ -41,11 +51,26 @@ class HomeViewModel: ObservableObject {
                 self?.statistics = returnedStats
             }
             .store(in: &cancellables)
+        
     }
     
     func reloadData() {
         isLoading = true
         coinDataService.getCoins()
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+                    coin.symbol.lowercased().contains(lowercasedText) ||
+                    coin.id.lowercased().contains(lowercasedText)
+        }
     }
     
     private func mapGlobalMarketData(marketDataModel: MarketDataModel?) -> [StatisticModel] {
